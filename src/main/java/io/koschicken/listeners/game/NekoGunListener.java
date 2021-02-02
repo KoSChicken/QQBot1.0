270b6400a7fe92f0d4c62424cb005fb9dd2ba5cf
KoSChicken <springhyq9421@outlook.com>
2021-02-02 17:34:10 +0800

    nekogun

--- src/main/java/io/koschicken/listeners/game/NekoGunListener.java
+++ src/main/java/io/koschicken/listeners/game/NekoGunListener.java
@@ -3,19 +3,12 @@ package io.koschicken.listeners.game;
 import com.forte.qqrobot.anno.Filter;
 import com.forte.qqrobot.anno.Listen;
 import com.forte.qqrobot.beans.messages.msgget.GroupMsg;
-import com.forte.qqrobot.beans.messages.result.GroupMemberInfo;
 import com.forte.qqrobot.beans.messages.types.MsgGetTypes;
 import com.forte.qqrobot.sender.MsgSender;
 import io.koschicken.constants.Constants;
-import io.koschicken.database.bean.Scores;
 import io.koschicken.database.service.impl.ScoresServiceImpl;
-import io.koschicken.utils.Utils;
-import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Collections;
-import java.util.List;
 
 @Component
 public class NekoGunListener {
@@ -28,35 +21,36 @@ public class NekoGunListener {
     public void nekoGun(GroupMsg msg, MsgSender sender) {
         String qq = msg.getQQ();
         String groupCode = msg.getGroupCode();
-        Scores score = scoresService.getById(qq);
-        if (score.getNekogun() > 0) {
-            score.setNekogun(score.getNekogun() - 1);
-            scoresService.updateById(score);
-            String message = Constants.CQ_AT + qq + "] 击中 ";
-            List<Scores> list = scoresService.listByGroupCode(groupCode);
-            Collections.shuffle(list);
-            Scores scores = list.get(0);
-            int i = RandomUtils.nextInt(5000, 50001);
-            int cri = RandomUtils.nextInt(1, 101);
-            boolean flag = false;
-            if (cri >= 80) {
-                i *= 2;
-                flag = true;
-            }
-            long max = Math.max(0, scores.getScore() - i);
-            scores.setScore(max);
-            scoresService.updateById(scores);
-            GroupMemberInfo info = sender.GETTER.getGroupMemberInfo(groupCode, scores.getQq());
-            String target = Utils.dealCard(info.getCard()) + "(" + info.getQQ() + ")";
-            message += target + "造成" + i + "点伤害";
-            if (flag) {
-                message += "（爆击！）";
-            }
-            message += "，" + target + "剩余生命值：" + max;
-            sender.SENDER.sendGroupMsg(groupCode, message);
-        } else {
-            sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "] 没子弹了，发送 #nekogun-r 消耗一万生命值恢复子弹。");
-        }
+        sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "love & peace");
+//        Scores score = scoresService.getById(qq);
+//        if (score.getNekogun() > 0) {
+//            score.setNekogun(score.getNekogun() - 1);
+//            scoresService.updateById(score);
+//            String message = Constants.CQ_AT + qq + "] 击中 ";
+//            List<Scores> list = scoresService.listByGroupCode(groupCode);
+//            Collections.shuffle(list);
+//            Scores scores = list.get(0);
+//            int i = RandomUtils.nextInt(5000, 50001);
+//            int cri = RandomUtils.nextInt(1, 101);
+//            boolean flag = false;
+//            if (cri >= 80) {
+//                i *= 2;
+//                flag = true;
+//            }
+//            long max = Math.max(0, scores.getScore() - i);
+//            scores.setScore(max);
+//            scoresService.updateById(scores);
+//            GroupMemberInfo info = sender.GETTER.getGroupMemberInfo(groupCode, scores.getQq());
+//            String target = Utils.dealCard(info.getCard()) + "(" + info.getQQ() + ")";
+//            message += target + "造成" + i + "点伤害";
+//            if (flag) {
+//                message += "（爆击！）";
+//            }
+//            message += "，" + target + "剩余生命值：" + max;
+//            sender.SENDER.sendGroupMsg(groupCode, message);
+//        } else {
+//            sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "] 没子弹了，发送 #nekogun-r 消耗一万生命值恢复子弹。");
+//        }
     }
 
     @Listen(MsgGetTypes.groupMsg)
@@ -64,17 +58,18 @@ public class NekoGunListener {
     public void nekoGunReload(GroupMsg msg, MsgSender sender) {
         String qq = msg.getQQ();
         String groupCode = msg.getGroupCode();
-        Scores score = scoresService.getById(qq);
-        Long s = score.getScore();
-        if (s > 10000) {
-            int nekogunRemain = score.getNekogun() + 10;
-            score.setNekogun(nekogunRemain);
-            score.setScore(s - 10000);
-            scoresService.updateById(score);
-            String message = Constants.CQ_AT + qq + "] 重新装填nekogun，剩余生命值：" + (s - 10000) + "，剩余子弹：" + nekogunRemain;
-            sender.SENDER.sendGroupMsg(groupCode, message);
-        } else {
-            sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "] 生命值不足");
-        }
+        sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "love & peace");
+//        Scores score = scoresService.getById(qq);
+//        Long s = score.getScore();
+//        if (s > 10000) {
+//            int nekogunRemain = score.getNekogun() + 10;
+//            score.setNekogun(nekogunRemain);
+//            score.setScore(s - 10000);
+//            scoresService.updateById(score);
+//            String message = Constants.CQ_AT + qq + "] 重新装填nekogun，剩余生命值：" + (s - 10000) + "，剩余子弹：" + nekogunRemain;
+//            sender.SENDER.sendGroupMsg(groupCode, message);
+//        } else {
+//            sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "] 生命值不足");
+//        }
     }
 }
