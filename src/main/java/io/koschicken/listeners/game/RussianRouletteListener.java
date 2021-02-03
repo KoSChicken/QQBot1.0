e5ca967a78a2c03e154535e11292ff9a80a537d6
KoSChicken <springhyq9421@outlook.com>
2021-02-03 15:36:22 +0800

    bugfix: 修复死者仍能继续开枪的bug

--- src/main/java/io/koschicken/listeners/game/RussianRouletteListener.java
+++ src/main/java/io/koschicken/listeners/game/RussianRouletteListener.java
@@ -9,13 +9,18 @@ import io.koschicken.constants.Constants;
 import io.koschicken.database.bean.Scores;
 import io.koschicken.database.service.impl.ScoresServiceImpl;
 import org.apache.commons.lang3.RandomUtils;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -43,26 +48,15 @@ public class RussianRouletteListener {
         String qq = msg.getQQ();
         String groupCode = msg.getGroupCode();
         List<String> deadList = dead.get(groupCode);
-        if (!CollectionUtils.isEmpty(deadList) && deadList.contains(qq)) {
-            sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "] 死人不要参加游戏。");
-        } else {
-            List<Boolean> bullets = gunMap.get(groupCode);
-            deadList = new CopyOnWriteArrayList<>();
-            if (bullets != null && bullets.contains(true)) {
-                LOGGER.info("{}群开枪前：{}", groupCode, printBullets(bullets));
-                int bulletCount = countBullets(bullets);
-                // 开枪
-                shot(sender, qq, groupCode, bullets, bulletCount, deadList);
+        if (!CollectionUtils.isEmpty(deadList)) {
+            if (deadList.contains(qq)) {
+                sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "] 死人不要参加游戏。");
             } else {
-                bullets = new CopyOnWriteArrayList<>();
-                int bulletCount = load(msg, bullets);
-                gunMap.put(groupCode, bullets);
-                LOGGER.info("{}群开枪前：{}", groupCode, printBullets(bullets));
-                sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "] 拿起手枪装填了" + bulletCount + "颗子弹，随后向自己的脑袋扣动了扳机。");
-                // 开枪
-                shot(sender, qq, groupCode, bullets, bulletCount, deadList);
+                shot(msg, sender, qq, groupCode, deadList);
             }
-            LOGGER.info("{}群开枪后：{}", groupCode, printBullets(bullets));
+        } else {
+            deadList = new CopyOnWriteArrayList<>();
+            shot(msg, sender, qq, groupCode, deadList);
         }
     }
 
@@ -82,12 +76,6 @@ public class RussianRouletteListener {
                 sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "] 游戏没有结束谁都不许下车。");
             }
         }
-    }
-
-    private String printBullets(List<Boolean> list) {
-        StringBuilder sb = new StringBuilder();
-        list.forEach(b -> sb.append(b).append(" "));
-        return sb.toString();
     }
 
     private int countBullets(List<Boolean> list) {
@@ -121,17 +109,32 @@ public class RussianRouletteListener {
         return bulletCount;
     }
 
+    private void shot(GroupMsg msg, MsgSender sender, String qq, String groupCode, List<String> deadList) {
+        List<Boolean> bullets = gunMap.get(groupCode);
+        if (bullets != null && bullets.contains(true)) {
+            int bulletCount = countBullets(bullets);
+            // 开枪
+            shot(sender, qq, groupCode, bullets, bulletCount, deadList);
+        } else {
+            bullets = new CopyOnWriteArrayList<>();
+            int bulletCount = load(msg, bullets);
+            gunMap.put(groupCode, bullets);
+            sender.SENDER.sendGroupMsg(groupCode, Constants.CQ_AT + qq + "] 拿起手枪装填了" + bulletCount + "颗子弹，随后向自己的脑袋扣动了扳机。");
+            // 开枪
+            shot(sender, qq, groupCode, bullets, bulletCount, deadList);
+        }
+        String bulletStr = StringUtils.collectionToDelimitedString(bullets, ", ");
+        LOGGER.info("{}群开枪后：{}", groupCode, bulletStr);
+    }
+
     private void shot(MsgSender sender, String qq, String groupCode, List<Boolean> bullets, int bulletCount, List<String> deadList) {
         Boolean bullet = bullets.get(0);
         int random = RandomUtils.nextInt(0, 1001);
         Map<String, Integer> map = continuous.get(groupCode);
         if (CollectionUtils.isEmpty(map)) {
-            map = new HashMap<>();
+            map = new ConcurrentHashMap<>();
         }
-        Integer shotCount = map.get(qq);
-        if (shotCount == null) {
-            shotCount = 0;
-        }
+        int shotCount = Objects.isNull(map.get(qq)) ? 0 : map.get(qq);
         String message;
         Scores score = scoresService.getById(qq);
         if (score == null) {
@@ -140,21 +143,8 @@ public class RussianRouletteListener {
         }
         if (Boolean.TRUE.equals(bullet)) {
             if (random < 986) {
-                bulletCount--;
-                bullets.remove(0);
-                message = Constants.CQ_AT + qq + "] 死了，币-" + (shotCount + 1) * 10000;
-                map.remove(qq);
-                score.setScore(score.getScore() - ((shotCount + 1) * 10000L) >= 0 ? score.getScore() - ((shotCount + 1) * 10000L) : 0);
-                scoresService.updateById(score);
-                deadList.add(qq);
-                if (bulletCount > 0) {
-                    dead.put(groupCode, deadList);
-                    message = message + "，还有" + bulletCount + "颗子弹。";
-                } else {
-                    message = message + "，游戏结束。";
-                    clear(groupCode);
-                }
-            } else if (random < 996){
+                message = dead(qq, groupCode, bullets, bulletCount, deadList, map, shotCount, score);
+            } else if (random < 996) {
                 message = Constants.CQ_AT + qq + "] 手里的枪卡壳了，还有" + bulletCount + "颗子弹。";
                 Collections.shuffle(bullets);
             } else {
@@ -163,17 +153,44 @@ public class RussianRouletteListener {
                 scoresService.updateById(score);
             }
         } else {
-            long rate = bullets.size() / (bullets.size() - bulletCount);
-            int reward = (int) Math.floor(10000D * rate);
-            bullets.remove(0);
-            score.setScore(Math.min(score.getScore() + reward, Long.MAX_VALUE));
-            scoresService.updateById(score);
-            shotCount++;
-            map.put(qq, shotCount);
-            continuous.put(groupCode, map);
-            message = Constants.CQ_AT + qq + "] 安然无恙，币+" + reward + "，还有" + bulletCount + "颗子弹。";
+            message = alive(qq, groupCode, bullets, bulletCount, map, shotCount, score);
         }
         sender.SENDER.sendGroupMsg(groupCode, message);
+    }
+
+    @NotNull
+    private String dead(String qq, String groupCode, List<Boolean> bullets, int bulletCount, List<String> deadList, Map<String, Integer> map, int shotCount, Scores score) {
+        String message;
+        bulletCount--;
+        bullets.remove(0);
+        message = Constants.CQ_AT + qq + "] 死了，币-" + (shotCount + 1) * 10000;
+        map.remove(qq);
+        score.setScore(score.getScore() - ((shotCount + 1) * 10000L) >= 0 ? score.getScore() - ((shotCount + 1) * 10000L) : 0);
+        scoresService.updateById(score);
+        deadList.add(qq);
+        if (bulletCount > 0) {
+            dead.put(groupCode, deadList);
+            message = message + "，还有" + bulletCount + "颗子弹。";
+        } else {
+            message = message + "，游戏结束。";
+            clear(groupCode);
+        }
+        return message;
+    }
+
+    @NotNull
+    private String alive(String qq, String groupCode, List<Boolean> bullets, int bulletCount, Map<String, Integer> map, int shotCount, Scores score) {
+        String message;
+        long rate = bullets.size() / (bullets.size() - bulletCount);
+        int reward = (int) Math.floor(10000D * rate);
+        bullets.remove(0);
+        score.setScore(Math.min(score.getScore() + reward, Long.MAX_VALUE));
+        scoresService.updateById(score);
+        shotCount++;
+        map.put(qq, shotCount);
+        continuous.put(groupCode, map);
+        message = Constants.CQ_AT + qq + "] 安然无恙，币+" + reward + "，还有" + bulletCount + "颗子弹。";
+        return message;
     }
 
     private void clear(String groupCode) {
